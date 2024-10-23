32fc1d9eb2e62abbf7c932d904f16c263e45001f
rasunM <rasunmanohara@gmail.com>
2024-10-23 22:20:51 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import {
+	About,
+	Experience,
+	Feedbacks,
+	Hero,
+	Navbar,
+	Tech,
+	Works,
+} from "./components";
+
+export default function Home() {
+	return (
+		<div className="relative z-0 bg-primary font-sans">
+			<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+				<Navbar />
+				<Hero />
+			</div>
+			<About />
+			<Experience />
+			<Tech />
+			<Works />
+			<Feedbacks />
+			
+		</div>
+	);
+}
